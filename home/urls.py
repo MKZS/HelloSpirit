28d08de486c512d428f5b1c30d294fb52009d6e5
Mark Smith <mark1122334455@gmail.com>
2015-11-10 22:22:24 +0000

    Initial creation of HelloSpirit Website

---
+++ home/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # This code was when we created templates from scratch
+    
+    # ex: /polls/
+    url(r'^$', views.home, name='index'),
+    # ex: /polls/5/
+    #url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    #url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]    
+    # The tutorial then led us to use the generic views system with the code:
+
+#urlpatterns = [
+    #url(r'^$', views.IndexView.as_view(), name='index'),
+    #url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    #url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+#]
